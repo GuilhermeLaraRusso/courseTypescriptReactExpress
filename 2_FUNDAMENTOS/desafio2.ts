02e325dbc7bb592e9560dc4c3321d4cfa8086242
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2023-01-25 14:09:35 -0300

    challange 2

---
+++ 2_FUNDAMENTOS/desafio2.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+const x: number = 10;
+
+const y: string = x.toString();
+
+console.log('tipo da variável y: ' + typeof y);
+
+console.log(`isso é uma string ${y}`);
+
+export default 1;
